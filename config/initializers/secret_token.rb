ecb5f349d169295ce1dab221a83652b8a9755f23
Jason Younker <jason@ynkr.org>
2011-06-25 16:05:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cri::Application.config.secret_token = 'cb5fa7665e9deb5303bae4e7d17c84646eea0942b050feb3e7d3746ef88db599bd322c31b3815b0aec93c2f95fd950c6e51298ceccd1708f8541397351f0128f'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Cri::Application.routes.draw do
+  root :to => 'index#index'
+  # match ':controller(/:action(/:id(.:format)))'
+end
