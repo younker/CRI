6599f017eb8ece2668c6a8d14ff448978aa1f3ce
Jason Younker <jason@ynkr.org>
2011-11-21 14:53:58 -0800

    Another attempt to fix sprite issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Cri::Application.configure do
   # Specify the default JavaScript compressor
   config.assets.js_compressor  = :uglifier
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
   # Generate digests for assets URLs
