ab5da01ec9039c9896702560d4c964af9deec056
Jason Younker <jason@ynkr.org>
2011-11-21 12:03:26 -0800

    Cap and deploy stuff

--- Gemfile
+++ Gemfile
@@ -17,10 +17,8 @@ gem 'cancan'
 
 gem "younker-string-extensions", "~> 0.0.2"
 gem "contact", "~> 0.0.11"
-# gem "blog", :git => 'git@github.com:younker/Blog.git', :tag => 'v1.0'
-gem "blog", :path => '/websites/gems/Blog'
+gem "blog", :git => 'git@github.com:younker/Blog.git', :tag => 'v1.0'
 gem "team", :git => 'git@github.com:younker/team.git', :tag => 'v0.1'
-# gem "team", :path => '/websites/gems/team'
 
 # Deploy with Capistrano
 # gem 'capistrano'
---
+++ config/deploy.rb
@@ -0,0 +1,104 @@
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# Setup (taken mostly from http://forum.webfaction.com/viewtopic.php?id=4906)
+# cap deploy:setup
+# cap deploy:check
+
+# New Repo Steps:
+#   gem install capistrano
+#   capify .
+#   cap deploy:setup
+#   cap deploy:check
+# Note: you may have to remove the following for the first cap deploy
+#   after "deploy:symlink", "deploy:bundle_install"
+
+
+set :app_name, "cri"
+set :webfaction_username, "cri"
+set :webfaction_port, "29299"
+
+set :webfaction_db_type, "mysql"
+set :webfaction_db, "cri_prod"
+set :webfaction_db_username, "cri_prod"
+# set :database_yml_template, "database.yml"
+
+set :deploy_to, "/home/#{webfaction_username}/webapps-releases/#{app_name}"
+ 
+set :scm, :git
+set :scm_user, "younker"
+set :scm_verbose, true
+set :repository, "git@github.com:younker/CRI.git"
+ 
+set :user, "#{webfaction_username}"
+set :use_sudo, false 
+ 
+set :domain, "#{webfaction_username}.webfactional.com"
+ 
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+ 
+default_environment['PATH']='/home/cri/webapps/git/bin:/home/cri/webapps/cri/bin:/home/cri/bin:/usr/local/bin:/usr/local/mysql/bin:/opt/local/apache2/bin:/opt/local/bin:/Users/younker/.rvm/gems/ruby-1.8.7-p302/bin:/usr/kerberos/bin:/bin:/usr/bin'
+default_environment['GEM_HOME']='/home/cri/webapps/cri/gems'
+
+# Basic Capistrano Deployment Steps:
+#   deploy:update_code
+#   deploy:symlink
+#   deploy:restart
+namespace :deploy do
+  desc "Symlink all the /shared assets to their new version-specific path"
+  task :symlink do
+    # release_path = /home/cri/webapps-releases/cri/releases/20110407164415
+    # deploy_to    = /home/cri/webapps-releases/cri
+    run "ln -nfs #{release_path} #{deploy_to}/current"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/contact.yml #{release_path}/config/contact.yml"
+    run "ln -nfs #{shared_path}/config/ecommerce.yml #{release_path}/config/ecommerce.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/"
+    run "ln -nfs #{shared_path}/certs #{release_path}"
+  end
+
+  # A cron job is created to restart the nginx server every 20 minutes if it is not already running.
+  #    To restart nginx, run: ~/webapps/<app_name>/bin/restart
+  #    To start nginx, run: ~/webapps/<app_name>/bin/start
+  #    To stop nginx, run: ~/webapps/<app_name>/bin/stop
+  desc "Redefine deploy:restart"
+  task :restart, :roles => :app do
+    run "/home/#{webfaction_username}/webapps/#{app_name}/bin/restart"
+  end
+  # TODO younker [2011-04-19 10:05]
+  # For some reasons, deploy:restart is NOT firing after deploy:symlink. Figure out why, but until then...
+  after "deploy:bundle_install", "deploy:restart"
+
+  desc "Redefine deploy:start"
+  task :start, :roles => :app do
+    run "/home/#{webfaction_username}/webapps/#{app_name}/bin/start"
+  end
+ 
+  desc "Redefine deploy:stop"
+  task :stop, :roles => :app do
+    run "/home/#{webfaction_username}/webapps/#{app_name}/bin/stop"
+  end
+
+
+  ##
+  ## Custom Tasks
+  ##
+  desc "bundle install the necessary prerequisites"
+  task :bundle_install, :roles => :app do
+    run "cd #{deploy_to}/current/ && /home/cri/webapps/#{app_name}/bin/bundle install"
+  end
+  # after "deploy:symlink", "deploy:bundle_install"
+end
+
+
+
+# task :staging do
+#   role :web, 'staging.whoz.in'
+#   role :app, 'staging.whoz.in'
+#   role :db, 'staging.whoz.in', :primary => true
+# end
+# 
+# task :production do
+# end
