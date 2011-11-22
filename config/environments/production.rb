ceb6b59b19cafdcf61747c7b29f3269a016b4bd6
Jason Younker <jason@ynkr.org>
2011-11-21 16:18:38 -0800

    Assets bug: tweak, commit, test, repeat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,10 @@ Cri::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  # config.assets.compress = true
+  config.assets.compress = true
 
   # Specify the default JavaScript compressor
-  # config.assets.js_compressor  = :uglifier
+  config.assets.js_compressor  = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
